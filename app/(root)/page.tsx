73c4cc54aa4b16358116743f491735c9b46853fe
Hernan Acero <contacto.acero.hernan@gmail.com>
2026-03-19 22:05:13 -0500

    feat: add new page added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import { BookCard } from "@/components/BookCard";
 import { HeroSection } from "@/components/HeroSection";
-import { sampleBooks } from "@/lib/contants";
+import { sampleBooks } from "@/lib/constants";
 import React from "react";
 
 const Page = (): React.ReactElement => {
--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,4 +1,4 @@
-import { SampleBook } from "@/lib/contants";
+import { SampleBook } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
