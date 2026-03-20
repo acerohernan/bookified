0c177f2600bc85ca51d0d39657907bb7b20a3cf1
Hernan Acero <contacto.acero.hernan@gmail.com>
2026-03-19 20:50:44 -0500

    feat: navbar created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Serif, Mona_Sans } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/Navbar";
 
 const ibmPlexSerif = IBM_Plex_Serif({
   variable: "--font-ibm-plex-serif",
@@ -31,7 +32,10 @@ export default function RootLayout({
       lang="en"
       className={`${ibmPlexSerif.variable} ${monaSans.variable} relative font-sans h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const navItems = [
+  {
+    label: "Library",
+    href: "/",
+  },
+  {
+    label: "Add New",
+    href: "/books/new",
+  },
+];
+
+export const Navbar = (): React.ReactElement => {
+  const pathName = usePathname();
+  return (
+    <header className="w-full fixed z-50 bg-('--bg-primary')">
+      <div className="wrapper navbar-height py-4 flex justify-between items-center">
+        <Link href="/" className="flex gap-0.5 items-center">
+          <Image
+            src="/assets/logo.png"
+            alt="Bookified"
+            width={42}
+            height={26}
+          />
+          <span className="logo-text">Bookified</span>
+        </Link>
+
+        <nav className="w-fit flex gap-7.5 items-center">
+          {navItems.map(({ href, label }) => {
+            const isActive =
+              pathName === href || (href !== "/" && pathName.startsWith(href));
+
+            return (
+              <Link
+                key={label}
+                href={href}
+                className={cn(
+                  "nav-link-base",
+                  isActive ? "nav-link-active" : "text-black hover:opacity-70",
+                )}
+              >
+                {label}
+              </Link>
+            );
+          })}
+        </nav>
+      </div>
+    </header>
+  );
+};
